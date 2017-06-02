f6f48f07144158ebdd447dd61fc3eb1e01ecee81
Ivan Porto Carrero <ivan@flanders.co.nz>
2017-06-02 08:46:12 -0700

    allow for timeouts

--- bus.go
+++ bus.go
@@ -1,7 +1,6 @@
 package eventbus
 
 import (
-	"log"
 	"sync"
 	"time"
 
@@ -17,38 +16,67 @@ type Event struct {
 }
 
 // NOOPHandler drops events on the floor without taking action
-var NOOPHandler = HandlerWithError(func(_ Event) error { return nil }, nil)
+var NOOPHandler = Handler(func(_ Event) error { return nil })
 
-type defaultEventHandler struct {
-	on    func(Event) error
-	onErr func(error)
-}
-
-func (h *defaultEventHandler) On(evt Event) error {
-	return h.on(evt)
-}
-
-func (h *defaultEventHandler) OnFailed(err error) {
-	if h.on != nil {
-		h.OnFailed(err)
+// Handler wraps a function that will be called when an event is received
+// In this mode the handler is quiet when an error is produced by the handler
+// so the user of the eventbus needs to handle that error
+func Handler(on func(Event) error) EventHandler {
+	return &defaultHandler{
+		on: on,
 	}
 }
 
-func Handler(handler func(Event) error) EventHandler {
-	return HandlerWithError(handler, func(err error) { log.Println(err) })
+type defaultHandler struct {
+	on func(Event) error
 }
 
-func HandlerWithError(handler func(Event) error, errorHandler func(error)) EventHandler {
-	return &defaultEventHandler{
-		on:    handler,
-		onErr: errorHandler,
+// On event trigger
+func (h *defaultHandler) On(event Event) error {
+	return h.on(event)
+}
+
+func newSubscription(handler EventHandler, errorHandler func(error)) *eventSubcription {
+	return &eventSubcription{
+		handler: handler,
+		once:    new(sync.Once),
+		onError: errorHandler,
 	}
 }
 
-// EventHander
+type eventSubcription struct {
+	listener chan Event
+	handler  EventHandler
+	once     *sync.Once
+	onError  func(error)
+}
+
+func (e *eventSubcription) Listen() {
+	e.once.Do(func() {
+		e.listener = make(chan Event)
+		go func() {
+			for evt := range e.listener {
+				if err := e.handler.On(evt); err != nil {
+					e.onError(err)
+				}
+			}
+		}()
+	})
+}
+
+func (e *eventSubcription) Stop() {
+	close(e.listener)
+	e.listener = nil
+	e.once = new(sync.Once)
+}
+
+func (e *eventSubcription) Matches(handler EventHandler) bool {
+	return e.handler == handler
+}
+
+// EventHandler deals with handling events
 type EventHandler interface {
 	On(Event) error
-	OnFailed(error)
 }
 
 type filteredHandler struct {
@@ -60,11 +88,7 @@ func (f *filteredHandler) On(evt Event) error {
 	if !f.Matches(evt) {
 		return nil
 	}
-	return f.On(evt)
-}
-
-func (f *filteredHandler) OnFailed(err error) {
-	f.OnFailed(err)
+	return f.Next.On(evt)
 }
 
 // EventPredicate for filtering events
@@ -90,28 +114,35 @@ type EventBus interface {
 type defaultEventBus struct {
 	lock *sync.RWMutex
 
-	channel  chan Event
-	handlers []EventHandler
-	closing  chan chan struct{}
-	log      logrus.FieldLogger
+	channel      chan Event
+	handlers     []*eventSubcription
+	closing      chan chan struct{}
+	log          logrus.FieldLogger
+	errorHandler func(error)
 }
 
 // New event bus with specified logger
 func New(log logrus.FieldLogger) EventBus {
+	return NewWithTimeout(log, 100*time.Millisecond)
+}
+
+// NewWithTimeout creates a new eventbus with a timeout after which an event handler gets cancelled
+func NewWithTimeout(log logrus.FieldLogger, timeout time.Duration) EventBus {
 	if log == nil {
 		log = logrus.New().WithFields(nil)
 	}
 	e := &defaultEventBus{
-		closing: make(chan chan struct{}),
-		channel: make(chan Event, 100),
-		log:     log,
-		lock:    new(sync.RWMutex),
+		closing:      make(chan chan struct{}),
+		channel:      make(chan Event, 100),
+		log:          log,
+		lock:         new(sync.RWMutex),
+		errorHandler: func(err error) { log.Errorln(err) },
 	}
-	go e.dispatcherLoop()
+	go e.dispatcherLoop(timeout)
 	return e
 }
 
-func (e *defaultEventBus) dispatcherLoop() {
+func (e *defaultEventBus) dispatcherLoop(timeout time.Duration) {
 	totWait := new(sync.WaitGroup)
 	for {
 		select {
@@ -133,15 +164,17 @@ func (e *defaultEventBus) dispatcherLoop() {
 				var wg sync.WaitGroup
 				wg.Add(noh)
 				e.log.Debugf("notifying %d listeners", noh)
-				e.log.Debugln(e.handlers)
 				for _, handler := range e.handlers {
-					e.log.Debugln("notifying", handler)
-					go func(handler EventHandler) {
-						if err := handler.On(evt); err != nil {
-							handler.OnFailed(err)
+					go func(listener chan<- Event) {
+						timer := time.NewTimer(timeout)
+						select {
+						case listener <- evt:
+							timer.Stop()
+						case <-timer.C:
+							e.log.Warnf("Failed to send event %+v to listener within %v", evt, timeout)
 						}
 						wg.Done()
-					}(handler)
+					}(handler.listener)
 				}
 
 				wg.Wait()
@@ -150,10 +183,12 @@ func (e *defaultEventBus) dispatcherLoop() {
 			})
 		case closed := <-e.closing:
 			totWait.Wait()
-
-			e.lock.Lock()
-			e.handlers = nil
 			close(e.channel)
+			e.lock.Lock()
+			for _, h := range e.handlers {
+				h.Stop()
+			}
+			e.handlers = nil
 			e.lock.Unlock()
 
 			closed <- struct{}{}
@@ -163,15 +198,12 @@ func (e *defaultEventBus) dispatcherLoop() {
 	}
 }
 
-func (e *defaultEventBus) dispatchEventWithTimeout(channel chan<- Event, timeout time.Duration, event Event, wg *sync.WaitGroup) {
-	timer := time.NewTimer(timeout) // uses timer so it can be stopped and cleaned up
-	select {
-	case channel <- event:
-		timer.Stop()
-	case <-timer.C:
-		e.log.Warnf("Failed to send event %+v to listener within %v", event, timeout)
-	}
-	wg.Done()
+// SetErrorHandler changes the default error handler which logs as error
+// to the new error handler provided to this method
+func (e *defaultEventBus) SetErrorHandler(handler func(error)) {
+	e.lock.Lock()
+	e.errorHandler = handler
+	e.lock.Unlock()
 }
 
 // Publish an event to all interested subscribers
@@ -179,26 +211,30 @@ func (e *defaultEventBus) Publish(evt Event) {
 	e.channel <- evt
 }
 
+// Subscribe to events published in the bus
 func (e *defaultEventBus) Subscribe(handlers ...EventHandler) {
 	e.lock.Lock()
 	e.log.Debugf("adding %d listeners", len(handlers))
-	e.handlers = append(e.handlers, handlers...)
+	for _, handler := range handlers {
+		sub := newSubscription(handler, e.errorHandler)
+		e.handlers = append(e.handlers, sub)
+		sub.Listen()
+	}
 	e.lock.Unlock()
 }
 
 func (e *defaultEventBus) Unsubscribe(handlers ...EventHandler) {
-	e.lock.RLock()
+	e.lock.Lock()
 	if len(e.handlers) == 0 {
 		e.log.Debugf("nothing to remove from", len(handlers))
-		e.lock.RUnlock()
+		e.lock.Unlock()
 		return
 	}
-	e.lock.RUnlock()
-	e.lock.Lock()
 	e.log.Debugf("removing %d listeners", len(handlers))
 	for _, h := range handlers {
 		for i, handler := range e.handlers {
-			if h == handler {
+			if handler.Matches(h) {
+				handler.Stop()
 				// replace handler because it will still process messages in flight
 				e.handlers = append(e.handlers[:i], e.handlers[i+1:]...)
 				break
@@ -209,6 +245,7 @@ func (e *defaultEventBus) Unsubscribe(handlers ...EventHandler) {
 }
 
 func (e *defaultEventBus) Close() error {
+	e.log.Debugf("closing eventbus")
 	ch := make(chan struct{})
 	e.closing <- ch
 	<-ch
@@ -218,6 +255,7 @@ func (e *defaultEventBus) Close() error {
 }
 
 func (e *defaultEventBus) Len() int {
+	e.log.Debugf("getting the length of the handlers")
 	e.lock.RLock()
 	sz := len(e.handlers)
 	e.lock.RUnlock()
